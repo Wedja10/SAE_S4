5bffa845c51e179a487ae5489a1c9a327a47e205
mickeyilestmignon <achireceseiandrei@gmail.com>
2025-02-13 17:30:24 +0100

    Lobby page

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./view/Home";
+import Lobby from "./view/Lobby.tsx";
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/lobby" element={<Lobby />} />
       </Routes>
     </Router>
   );
