edb34aabbc3c7a6f294094ca958c69ba107254f7
Abdelrmb <richeabdelrahim1@gmail.com>
2025-02-13 11:21:33 +0100

    style: format code for consistency in AppRouter component

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./view/Home";
 
 const AppRouter: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRouter;
