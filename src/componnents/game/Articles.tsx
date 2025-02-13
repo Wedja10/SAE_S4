bf031ab1f89e9c71446119ff356e88a220ea0462
Abdelrmb <richeabdelrahim1@gmail.com>
2025-02-13 17:13:46 +0100

    feat: enhance Articles and WikiView components with improved styles and scrollbar functionality

--- src/componnents/game/Articles.tsx
+++ src/componnents/game/Articles.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import '../../style/game/Articles.css';
 
 const Articles: React.FC = () => {
-    const articlesToFind = ["Article 1", "Article 2", "Article 3", "Article 4", "Article 5"];
-    const visitedArticles = ["Article 1", "Article 2", "Article 3"];
+    const articlesToFind = ["Article 1", "Article 2", "Article 3", "Article 4", "Article 5", "Article 2", "Article 2", "Article 2"];
+    const visitedArticles = ["Article 1", "Article 2", "Article 3", "Article 3", "Article 3", "Article 3", "Article 3", "Article 3", "Article 3"];
   
     return (
       <div className="articles-container">
@@ -11,7 +11,10 @@ const Articles: React.FC = () => {
           <h2 className="articles-title">Articles à Trouver</h2>
           <ul className="articles-list">
             {articlesToFind.map((article, index) => (
-              <li key={index} className="article-item article-to-find">{article}</li>
+              <React.Fragment key={index}>
+                <li className="article-item article-to-find">{article}</li>
+                <hr />
+              </React.Fragment>
             ))}
           </ul>
         </div>
@@ -20,7 +23,10 @@ const Articles: React.FC = () => {
           <h2 className="articles-title">Articles Visités</h2>
           <ul className="articles-list">
             {visitedArticles.map((article, index) => (
-              <li key={index} className="article-item article-visited">{article}</li>
+              <React.Fragment key={index}>
+                <li className="article-item article-visited">{article}</li>
+                <hr />
+              </React.Fragment>
             ))}
           </ul>
         </div>
--- src/componnents/game/WikiView.tsx
+++ src/componnents/game/WikiView.tsx
@@ -4,10 +4,6 @@ import '../../style/game/WikiView.css';
 const WikiView: React.FC = () => {
     return (
         <div className="wiki-container">
-            <h2 className="wiki-title">Ulrich Obrecht</h2>
-            <p className="wiki-content">
-                Ulrich Obrecht est un jurisconsulte et philologue français, né à Strasbourg le 23 juillet 1646...
-            </p>
         </div>
     );
 };
