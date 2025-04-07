a9d096118354844739f3328958005d14f31b5aea
Abdelrmb <richeabdelrahim1@gmail.com>
2025-04-07 15:01:36 +0200

    Refactor Navbar component to implement a responsive menu toggle feature, enhancing mobile navigation experience.

--- src/componnents/Navbar.tsx
+++ src/componnents/Navbar.tsx
@@ -1,15 +1,30 @@
 import { NavLink } from 'react-router-dom'
 import '../style/Navbar.css'
+import { useState } from 'react'
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <>
             <nav className="fade-in">
-                <ul className="nav-links">
-                    <li><NavLink to="/">HOME</NavLink></li>
-                    <li><NavLink to="/daily-challenge">DAILY</NavLink></li>
-                </ul>
+                <div className="nav-container">
+                    <div className="menu-toggle" onClick={toggleMenu}>
+                        <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+                        <li><NavLink to="/" onClick={() => setIsMenuOpen(false)}>HOME</NavLink></li>
+                        <li><NavLink to="/daily-challenge" onClick={() => setIsMenuOpen(false)}>DAILY</NavLink></li>
+                    </ul>
+                </div>
             </nav>
         </>
     )
